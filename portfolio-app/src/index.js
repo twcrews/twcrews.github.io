ec4ee2e32bca9116376922123819632d0b5c85fd
Tommy Crews <twcrews96@gmail.com>
2021-01-12 00:07:43 -0600

    Added theming.

--- portfolio-app/src/index.js
+++ portfolio-app/src/index.js
@@ -2,10 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import { blue, pink } from '@material-ui/core/colors';
+
+let theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#6aebff',
+      main: '#00b9ff',
+      dark: '#0089cb',
+      contrastText: '#fff',
+  },
+    secondary: pink
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
