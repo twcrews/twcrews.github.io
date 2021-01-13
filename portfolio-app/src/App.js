8ed764a8418729704ded6524e758a56b40e5a19c
Tommy Crews <twcrews96@gmail.com>
2021-01-12 22:12:44 -0600

    Added more page content.

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -1,29 +1,58 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import * as Material from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
 import { Logo } from './logo';
 
 function App() {
+  const [scrolled, setScrolled] = useState(false);
 
-  const useStyles = makeStyles()
+  const handleScroll = () => {
+    setScrolled(window.pageYOffset)
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll());
+    return function cleanup() {
+      window.removeEventListener('scroll', handleScroll());
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <div className="Layout">
-      <Material.AppBar position="static">
+      <Material.AppBar position="sticky" elevation={scrolled ? 3 : 0}>
         <Material.Toolbar className="NavBar">
-          <Logo />
-          <Material.Typography 
-            variant="h6" 
-          >
-            Tommy Crews
-          </Material.Typography>
-          <Material.Button color="inherit">
-            Login
-          </Material.Button>
+          <span className="NavItems">
+            <Logo />
+            <Material.Typography
+              variant="h6"
+            >
+              Tommy Crews
+            </Material.Typography>
+          </span>
+          <span className="NavItems">
+            <Material.Button color="inherit">
+              About
+            </Material.Button>
+            <Material.Button color="inherit">
+              Portfolio
+            </Material.Button>
+            <Material.Button
+              color="secondary"
+              variant="contained"
+            >
+              Contact
+            </Material.Button>
+          </span>
         </Material.Toolbar>
       </Material.AppBar>
-      <div />
+      <div>
+        <div
+          className="AboutSection"
+        >
+          <Material.Typography variant="h1">Hello, world!</Material.Typography>
+        </div>
+      </div>
     </div>
   );
 }
