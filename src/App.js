97996ed59a93ab755a16eb138210b812d61c527d
Tommy Crews <twcrews96@gmail.com>
2021-01-31 11:07:32 -0600

    UI enhancements.

--- src/App.js
+++ src/App.js
@@ -12,23 +12,36 @@ function App() {
   const [formData, setFormData] = useState({});
   const [formErrors, setFormErrors] = useState(false);
 
+  const fieldValid = (field) => {
+    var val = formData[field.ID];
+    var pattern = new RegExp(field.Pattern);
+    if (val) {
+      return val.trim().length > 0 && pattern.test(val.trim());
+    }
+  }
+
   const formValid = () => {
-    var valid = true;
-    Data.Contact.Fields.forEach(field => {
-      if (!formData[field.ID]) {
-        valid = false;
-      }
-    });
-    return valid;
+    var BreakException = {};
+    try {
+      Data.Contact.Fields.forEach(field => {
+        if (!fieldValid(field)) {
+          throw BreakException;
+        }
+      });
+    } catch (e) {
+      if (e !== BreakException) throw e;
+      return false;
+    }
+    return true;
   };
 
   const submitForm = () => {
 
   }
 
-  const handleLink = (url) => { window.open(url, "_blank") };
+  const handleLink = (url) => { window.open(url, "_blank"); };
   const handleFieldChange = (event) => {
-    var tmpForm = {...formData};
+    var tmpForm = { ...formData };
     tmpForm[event.target.id ?? event.target.name] = event.target.value;
     setFormData(tmpForm);
   };
@@ -38,7 +51,7 @@ function App() {
     } else {
       submitForm();
     }
-  }
+  };
 
   useScrollPosition(({ _prevPos, currPos }) => {
     const isScrolled = currPos.y < 0;
@@ -58,253 +71,267 @@ function App() {
   });
 
   return (
-    <React.Fragment>
-      <div
-        id="top"
-        className="Top"
-      />
-      <Material.AppBar
-        id="nav"
-        elevation={shadowNav ? 3 : 0}
-        className="AppBar"
-        position="sticky"
-      >
-        <Material.Toolbar className="NavBar">
-          <span className="NavItems">
-            <img
-              className="NavLogo"
-              alt="Logo"
-              src={Data.Meta.Logo}
-            />
-            <Material.Typography
-              className="NavTitle"
-              variant="h6"
-            >
-              {Data.Meta.Owner}
-            </Material.Typography>
-          </span>
-          <span className="NavItems">
-            {Data.Meta.Anchors.map(anchor =>
-              <Material.Button
-                key={anchor.Name}
-                id={anchor.Name}
-                className="AnchorButton"
-                variant={anchor.Variant}
-                color={anchor.Color}
-                onClick={() => document
-                  .getElementById(anchor.Link)
-                  .scrollIntoView()}
-              >
-                {anchor.Name}
-              </Material.Button>
-            )}
-          </span>
-        </Material.Toolbar>
-      </Material.AppBar>
-      <div
-        className="AboutSection"
-      >
+    <div className="Page">
+      <div className="BodyWrap">
         <div
-          className="Hero"
-          style={{ backgroundImage: "url(" + Data.Header.Hero + ")" }}
+          id="top"
+          className="Top"
         />
-        <Material.Typography
-          variant="h1"
-          paragraph
-          className="Header"
+        <Material.AppBar
+          id="nav"
+          elevation={shadowNav ? 3 : 0}
+          className="AppBar"
+          position="sticky"
         >
-          {Data.Header.Title}
-        </Material.Typography>
-        <Material.Typography
-          variant="h4"
-          className="Subtitle"
-        >
-          {Data.Header.SubtitlePrefix}
-          <TextTransition
-            text={Data.Header.SubtitleItems[currentThing]}
-            inline
-          />
-        </Material.Typography>
-        <div className="HeaderSpacer" />
-      </div>
-      <div className="AttributesSection Content">
-        <div className="Attributes">
-          {Data.Attributes.map(attr =>
-            <div
-              key={attr.Name}
-              className="AttributeTile"
-            >
-              <span className="BigIcon">
-                <Icon fontSize="inherit" color="primary">
-                  {attr.Icon}
-                </Icon>
-                <Material.Typography variant="h5">
-                  {attr.Name}
-                </Material.Typography>
-              </span>
-              <div className="Multiline GrayText">
-                <Material.Typography color="inherit">
-                  {attr.Description}
-                </Material.Typography>
-                <Material.Typography variant="h6" paragraph />
-                {attr.Sections.map(section =>
-                  <React.Fragment key={section.Title}>
-                    <Material.Typography variant="h6">
-                      {section.Title}
-                    </Material.Typography>
-                    {section.Content.map(item =>
-                      <Material.Typography key={item}>
-                        {item}
-                      </Material.Typography>
-                    )}
-                    <Material.Typography variant="h6" paragraph />
-                  </React.Fragment>
-                )}
-              </div>
-            </div>
-          )}
-        </div>
-      </div>
-      <div className="Content" id="portfolio">
-        <Material.Typography 
-          variant="h3" 
-          paragraph
-          className="SectionHeader"
-        >
-          {Data.Portfolio.Title}
-        </Material.Typography>
-        <span className="GrayText">
-          <Material.Typography variant="subtitle1" paragraph>
-            {Data.Portfolio.Description}
-          </Material.Typography>
-        </span>
-        <div className="PortfolioTiles">
-          {Data.Portfolio.Projects.map(project =>
-            <div
-              key={project.Title}
-              className="PortfolioProject"
-              style={{
-                backgroundImage: "url(" + project.Image + ")",
-                backgroundColor: "#00b9ff",
-                backgroundSize: "cover",
-                backgroundPosition: "center"
-              }}
-            >
-              <div
-                className="Darken FullHeight"
-                onClick={() => project.Enabled ?
-                  handleLink(project.Link) :
-                  null}
+          <Material.Toolbar className="NavBar">
+            <span className="NavItems">
+              <img
+                className="NavLogo"
+                alt="Logo"
+                src={Data.Meta.Logo}
+              />
+              <Material.Typography
+                className="NavTitle"
+                variant="h6"
               >
-                <div className="ProjectTypography">
-                  <Material.Typography variant="h6">
-                    {project.Title}
+                {Data.Meta.Owner}
+              </Material.Typography>
+            </span>
+            <span className="NavItems">
+              {Data.Meta.Anchors.map(anchor =>
+                <Material.Button
+                  key={anchor.Name}
+                  id={anchor.Name}
+                  className="AnchorButton"
+                  variant={anchor.Variant}
+                  color={anchor.Color}
+                  onClick={() => document
+                    .getElementById(anchor.Link)
+                    .scrollIntoView()}
+                >
+                  {anchor.Name}
+                </Material.Button>
+              )}
+            </span>
+          </Material.Toolbar>
+        </Material.AppBar>
+        <div
+          className="AboutSection"
+        >
+          <div
+            className="Hero"
+            style={{ backgroundImage: "url(" + Data.Header.Hero + ")" }}
+          />
+          <Material.Typography
+            variant="h1"
+            paragraph
+            className="Header"
+          >
+            {Data.Header.Title}
+          </Material.Typography>
+          <Material.Typography
+            variant="h4"
+            className="Subtitle"
+          >
+            {Data.Header.SubtitlePrefix}
+            <TextTransition
+              text={Data.Header.SubtitleItems[currentThing]}
+              inline
+            />
+          </Material.Typography>
+          <div className="HeaderSpacer" />
+        </div>
+        <div className="AttributesSection Content">
+          <div className="Attributes">
+            {Data.Attributes.map(attr =>
+              <div
+                key={attr.Name}
+                className="AttributeTile"
+              >
+                <span className="BigIcon">
+                  <Icon fontSize="inherit" color="primary">
+                    {attr.Icon}
+                  </Icon>
+                  <Material.Typography variant="h5">
+                    {attr.Name}
                   </Material.Typography>
-                  <Material.Typography
-                    variant="caption"
-                    style={{ textTransform: "uppercase" }}
-                  >
-                    {project.Year}
+                </span>
+                <div className="Multiline GrayText">
+                  <Material.Typography color="inherit">
+                    {attr.Description}
                   </Material.Typography>
-                  <Material.Typography
-                    variant="subtitle2"
-                    style={{ opacity: 0.8 }}
-                  >
-                    {project.Description}
-                  </Material.Typography>
+                  <Material.Typography variant="h6" paragraph />
+                  {attr.Sections.map(section =>
+                    <React.Fragment key={section.Title}>
+                      <Material.Typography variant="h6">
+                        {section.Title}
+                      </Material.Typography>
+                      {section.Content.map(item =>
+                        <Material.Typography key={item}>
+                          {item}
+                        </Material.Typography>
+                      )}
+                      <Material.Typography variant="h6" paragraph />
+                    </React.Fragment>
+                  )}
                 </div>
               </div>
-            </div>
-          )}
+            )}
+          </div>
         </div>
-      </div>
-      <div className="Content" id="contact">
-        <Material.Typography
-          variant="h3" 
-          paragraph
-          className="SectionHeader"
-        >
-          {Data.Contact.Title}
-        </Material.Typography>
-        <span className="GrayText">
-          <Material.Typography variant="subtitle1">
-            {Data.Contact.Description}
+        <div className="Content" id="portfolio">
+          <Material.Typography
+            variant="h3"
+            paragraph
+            className="SectionHeader"
+          >
+            {Data.Portfolio.Title}
           </Material.Typography>
-        </span>
-        <div className="ContactFields">
-          {Data.Contact.Fields.map(field => 
-            <div 
-              key={field.ID}
-              style={{
-                gridColumnStart: field.GridColumnStart,
-                gridColumnEnd: "span " + field.GridColumnSpan
-              }}
-            >
-              {
-                field.Component === "TextField" ?
-                  <Material.TextField
-                    id={field.ID}
-                    type={field.Type}
-                    label={field.Label}
-                    required={field.Required}
-                    onChange={handleFieldChange}
-                    value={formData[field.ID] ?? ""}
-                    multiline={field.Multiline}
-                    rows={field.Rows}
-                    error={!formData[field.ID] && formErrors}
-                    helperText = {formErrors && !formData[field.ID] ?
-                       field.HelperText : null}
-                    fullWidth
-                    variant="outlined"
-                  /> :
-                  field.Component === "Select" ?
-                  <Material.FormControl 
-                    id={field.ID}
-                    variant="filled" 
-                    fullWidth
-                    required={field.Required}
-                    error={!formData[field.ID] && formErrors}
-                  >
-                    <Material.InputLabel 
-                      id={field.ID + "-label"}
+          <span className="GrayText">
+            <Material.Typography variant="subtitle1" paragraph>
+              {Data.Portfolio.Description}
+            </Material.Typography>
+          </span>
+          <div className="PortfolioTiles">
+            {Data.Portfolio.Projects.map(project =>
+              <div
+                key={project.Title}
+                className="PortfolioProject"
+                style={{
+                  backgroundImage: "url(" + project.Image + ")",
+                  backgroundColor: "#00b9ff",
+                  backgroundSize: "cover",
+                  backgroundPosition: "center"
+                }}
+              >
+                <div
+                  className="Darken FullHeight"
+                  onClick={() => project.Enabled ?
+                    handleLink(project.Link) :
+                    null}
+                >
+                  <div className="ProjectTypography">
+                    <Material.Typography variant="h6">
+                      {project.Title}
+                    </Material.Typography>
+                    <Material.Typography
+                      variant="caption"
+                      style={{ textTransform: "uppercase" }}
                     >
-                      {field.Label}
-                    </Material.InputLabel>
-                    <Material.Select
-                      id={field.ID}
-                      name={field.ID}
-                      labelId={field.ID + "-label"}
-                      value={formData[field.ID] ?? ""}
-                      onChange={handleFieldChange}
+                      {project.Year}
+                    </Material.Typography>
+                    <Material.Typography
+                      variant="subtitle2"
+                      style={{ opacity: 0.8 }}
                     >
-                      {field.Items.map(item =>
-                        <Material.MenuItem
-                          key={item}
-                          value={item}
-                        >
-                          {item}
-                        </Material.MenuItem>
-                      )}
-                    </Material.Select>
-                    <Material.FormHelperText>
-                      {formErrors && !formData[field.ID] ?
-                       field.HelperText : null}
-                    </Material.FormHelperText>
-                  </Material.FormControl> : null
-              }
-            </div>
-          )}
+                      {project.Description}
+                    </Material.Typography>
+                  </div>
+                </div>
+              </div>
+            )}
+          </div>
         </div>
-        <Material.Button
-          variant="contained"
-          color="secondary"
-          size="large"
-          onClick={handleSendClick}
-        >
-          {Data.Contact.Button}
-        </Material.Button>
+        <div className="Content" id="contact">
+          <Material.Typography
+            variant="h3"
+            paragraph
+            className="SectionHeader"
+          >
+            {Data.Contact.Title}
+          </Material.Typography>
+          <span className="GrayText">
+            <Material.Typography variant="subtitle1">
+              {Data.Contact.Description}
+            </Material.Typography>
+          </span>
+          <div className="ContactFields">
+            {Data.Contact.Fields.map(field =>
+              <div
+                key={field.ID}
+                style={{
+                  gridColumnStart: field.GridColumnStart,
+                  gridColumnEnd: "span " + field.GridColumnSpan
+                }}
+              >
+                {
+                  field.Component === "TextField" ?
+                    <Material.TextField
+                      id={field.ID}
+                      type={field.Type}
+                      label={field.Label}
+                      required={field.Required}
+                      onChange={handleFieldChange}
+                      value={formData[field.ID] ?? ""}
+                      multiline={field.Multiline}
+                      rows={field.Rows}
+                      error={formErrors && !fieldValid(field)}
+                      helperText={formErrors && !fieldValid(field) ?
+                        field.HelperText : null}
+                      fullWidth
+                      variant="outlined"
+                    /> :
+                    field.Component === "Select" ?
+                      <Material.FormControl
+                        id={field.ID}
+                        variant="filled"
+                        fullWidth
+                        required={field.Required}
+                        error={formErrors && !fieldValid(field)}
+                      >
+                        <Material.InputLabel
+                          id={field.ID + "-label"}
+                        >
+                          {field.Label}
+                        </Material.InputLabel>
+                        <Material.Select
+                          id={field.ID}
+                          name={field.ID}
+                          labelId={field.ID + "-label"}
+                          value={formData[field.ID] ?? ""}
+                          onChange={handleFieldChange}
+                        >
+                          {field.Items.map(item =>
+                            <Material.MenuItem
+                              key={item}
+                              value={item}
+                            >
+                              {item}
+                            </Material.MenuItem>
+                          )}
+                        </Material.Select>
+                        <Material.FormHelperText>
+                          {formErrors && !fieldValid(field) ?
+                            field.HelperText : null}
+                        </Material.FormHelperText>
+                      </Material.FormControl> : null
+                }
+              </div>
+            )}
+          </div>
+          <Material.Button
+            variant="contained"
+            color="secondary"
+            size="large"
+            onClick={handleSendClick}
+          >
+            {Data.Contact.Button}
+          </Material.Button>
+        </div>
       </div>
-    </React.Fragment>
+      <footer className="Footer">
+        <div className="Content GrayText">
+          <Material.Typography paragraph>
+            {Data.Footer.Copyright.replace("{year}", new Date().getFullYear())}
+          </Material.Typography>
+          <Material.Button 
+            onClick={() => handleLink(Data.Footer.PromotionLink)}
+          >
+            {Data.Footer.PromotionText}
+          </Material.Button>
+        </div>
+      </footer>
+    </div>
   );
 }
 
